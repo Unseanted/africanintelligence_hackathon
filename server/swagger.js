145d72ccb460964781ec426090fd3f4f86a08bea
Perez-salem Tuwan <perezjoshua1811@gmail.com>
2025-08-14 12:02:27 +0100

    fix: swagger in prod

--- server/swagger.js
+++ server/swagger.js
@@ -1,6 +1,6 @@
 // swaggerOptions.js (or directly in app.js)
 
-const { port } = require('./configs/config.js'); // Ensure PORT is defined
+const { port, isProduction } = require('./configs/config.js'); // Ensure PORT is defined
 const swaggerOptions = {
   definition: {
     openapi: "3.0.0", // Specify the OpenAPI version
@@ -9,17 +9,18 @@ const swaggerOptions = {
       version: "1.0.0",
       description: "API documentation for the Learning Management System",
     },
-    servers: [
+    servers: isProduction ? [
       {
         url: `http://localhost:${port}/api`,
         description: "Development server",
-      },
-      {
+      },]
+      :
+      [{
       	url: `https://africanintelligence-hackathon-backend.onrender.com/api`,
       	description: "Swagger in prod"
-      }
+      }],
       // You can add more server URLs for production, staging, etc.
-    ],
+    
     components: {
       securitySchemes: {
         bearerAuth: {
